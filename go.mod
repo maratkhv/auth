106b31ebcedea322df43bb50bb7caec9c1574d07
khvmarat <marathvostov@gmail.com>
2024-11-03 20:05:57 +0300

    add tests for jwt package

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/maratkhv/protobuf v1.0.0
 	github.com/pressly/goose/v3 v3.22.1
 	github.com/redis/go-redis/v9 v9.6.1
+	github.com/stretchr/testify v1.9.0
 	golang.org/x/crypto v0.27.0
 	google.golang.org/grpc v1.66.1
 	google.golang.org/protobuf v1.34.2
@@ -21,6 +22,7 @@ require (
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -30,6 +32,7 @@ require (
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
--- internal/lib/jwt/jwt_test.go
+++ internal/lib/jwt/jwt_test.go
@@ -1,29 +1,90 @@
-package jwt_test
+package jwt
 
 import (
-	"auth/internal/lib/jwt"
+	"auth/internal/models"
+	"bytes"
 	"crypto/x509"
 	"encoding/pem"
 	"log/slog"
 	"testing"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGetPublicKey(t *testing.T) {
-	jwt.MustSetKeys(slog.Default())
-	key := jwt.GetPublicKey()
+func TestKeysGeneration(t *testing.T) {
+	setKeys()
+	key := GetPublicKey()
 
 	if key == nil || len(key) == 0 {
-		t.Error("failed to get key")
+		t.Fatal("failed to get key")
 	}
 
 	b, _ := pem.Decode(key)
 	if b == nil {
-		t.Error("failed to decode key")
+		t.Fatal("failed to decode key")
 	}
 
 	_, err := x509.ParsePKCS1PublicKey(b.Bytes)
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("failed to parse key: %v", err)
+	}
+}
+
+func TestToken(t *testing.T) {
+	setKeys()
+
+	tests := []struct {
+		name string
+		usr  *models.User
+		pwd  string
+	}{
+		{"normal", &models.User{Id: 1, Login: "user1"}, "123"},
+		{"normal2", &models.User{Id: 12, Login: "user12"}, "1233"},
 	}
 
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			tokenStr, err := NewToken(tc.usr, tc.pwd)
+			if err != nil {
+				t.Fatalf("failed to get token: %v", err)
+			}
+
+			key, err := jwt.ParseRSAPublicKeyFromPEM(GetPublicKey())
+			if err != nil {
+				t.Fatalf("failed to parse key: %v", err)
+			}
+
+			token, err := jwt.Parse(tokenStr, func(tk *jwt.Token) (interface{}, error) {
+				if _, ok := tk.Method.(*jwt.SigningMethodRSA); !ok {
+					t.Fatalf("signing method is not rsa: %T", tk.Method)
+				}
+				return key, nil
+			})
+			if err != nil {
+				t.Fatalf("failed to parse token")
+			}
+
+			claims, ok := token.Claims.(jwt.MapClaims)
+			if !ok {
+				t.Fatalf("wrong claims type: %T instead of jwt.MapClaims", token.Claims)
+			}
+
+			assert.Equal(t, tc.usr.Id, int64(claims["id"].(float64)))
+			assert.Equal(t, tc.pwd, claims["password"])
+			assert.Equal(t, tc.usr.Login, claims["login"])
+		})
+	}
+}
+
+func setKeys() bytes.Buffer {
+	var logBuffer bytes.Buffer
+	logger := slog.New(slog.NewJSONHandler(
+		&logBuffer,
+		&slog.HandlerOptions{},
+	))
+
+	MustSetKeys(logger)
+	return logBuffer
 }
